d8767bbcfc03b9f80411dd7f870f7dd40cf24ab4
Yana-best-student <yan16752009@yandex.ru>
2026-01-16 15:35:09 +0500

    Инициализация драйвера

---
+++ page/UiPage.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
+class UiPage:
+
+    def __init__(self, driver: WebDriver) -> None:
+        self.__url = "https://shop.mts.ru/"
+        self.__driver = driver
+
+    def go(self):
+        self.__driver.get(self.__url)
+
+
+
+
+
+
+   

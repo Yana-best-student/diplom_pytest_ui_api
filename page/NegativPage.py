e7b5c0a86f7153e073bcdcee08bf3b2d62c653a8
Yana-best-student <yan16752009@yandex.ru>
2026-01-22 05:31:13 +0500

    проверки проходят

---
+++ page/NegativPage.py
@@ -0,0 +1,52 @@
+import allure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+
+class NegativPage:
+
+    def __init__(self, browser: WebDriver) -> None:
+        self.__url = "https://shop.mts.ru/"
+        self.__driver = browser
+
+    def go(self):
+        self.__driver.get(self.__url)
+
+    #Передаем значение cookie
+    def set_cookie_policy(self):
+        cookie = {
+            "name": "COOKIES_MASSAGE_APPLY",
+            "value": "true"
+        }
+        self.__driver.add_cookie(cookie)
+
+    def search(self, term):
+        #Ожидаем появления поля поиска
+        (WebDriverWait(self.__driver, 10).
+         until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[name='q']"))))
+
+        self.__driver.find_element(
+            By.CSS_SELECTOR, "input[name='q']").click()
+
+        #Вводим в строку поиска название товара
+        (WebDriverWait(self.__driver, 10).
+         until(EC.visibility_of_element_located((By.XPATH, "//input[@id='search-popup-field']"))))
+        self.__driver.find_element(
+            By.XPATH, "//input[@id='search-popup-field']").send_keys(term)
+
+        self.__driver.find_element(
+            By.XPATH, "//span[text()='Найти']").click()
+        
+        novalid = WebDriverWait(self.__driver, 30).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, ".search-no-results-block__content .search-no-results-block__title"))
+            )
+        novalid_text = novalid.text
+        print(novalid_text)
+        
+        # Проверка наличия текста "Пока в корзине нет товаров..."
+        if "Ничего не нашлось" in novalid_text:
+            print("уточните запрос.")
